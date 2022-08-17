74863246c5b92ac0ad948356a3f4ecbb20f3f646
iLighty33 <iLighty33@gmail.com>
2022-08-17 21:20:35 +0500

    17.08.2022 - Classwork

--- 15.08.2022 Map/Source.cpp
+++ 15.08.2022 Map/Source.cpp
@@ -4,21 +4,98 @@
 #include <iostream>
 #include <chrono>
 #include <stdlib.h>
+#include <algorithm>
+#include <fstream>
+
+// Создать коллекцию пользователей на 10'000 
+// элементов
+// заполнить элементами коллекции map 
+// сравнить время необходимо на поиск в оригинальных
+// данных и временем поиска и составления map
+//
+// найти 10 пользователей с максимально близкими к средним зарплатам
+// записать в файл список пользователей(в режиме дозаписи)
 
 class User {
 public:
+	User() = default;
 	int id;
 	std::string name;
-	bool graphicOfWork = 0; // 1-day 0-night
-	int Salary = 0; // from 10 000 to 20 000 usd
-	//10 users - 
+	bool dayShift; // 0 - nigth, 1 - day
+	int salary; // зарплата
+	double temp;
 
-	User(int id, std::string name):id(id), name(name){}
+	User(int id, std::string name, bool dayShift, int salary) 
+		:id(id), 
+		name(name), 
+		dayShift(dayShift), 
+		salary(salary) {}
 
-	bool operator ==(const User& other) const {
+	bool operator==(const User& other)const {
 		return id == other.id && name == other.name;
 	}
+
+
+	
+	friend std::ostream& operator << (std::ostream& out, const User& obj) {
+		out << obj.id << ' ' << obj.name << ' ' << obj.dayShift << ' ' << obj.salary << ' ';
+		return out;
+	}
+
+	friend std::istream& operator>>(std::istream& in, User& obj) {
+		in >> obj.id >> obj.name >> obj.dayShift >> obj.salary;
+		return in;
+	}
 };
+
+void task(int count) {
+	std::vector<User> database;
+	database.reserve(count);
+	auto begin = std::chrono::steady_clock::now();
+	for (int i = 0; i < database.capacity(); i++)
+	{
+		database.emplace_back(i + 1, "user" + std::to_string(i + 1), static_cast<bool>(::rand()%2), 32000+ ::rand());
+	}
+	auto end = std::chrono::steady_clock::now();
+	auto elapsed_ms =
+		std::chrono::duration_cast<std::chrono::nanoseconds>
+		(end - begin);
+	std::cout << "create vector: " << elapsed_ms.count() << " ms\n";
+
+	std::map<std::string, User*> searchTree;
+
+	begin = std::chrono::steady_clock::now();
+	for (auto& user : database) {
+		searchTree.insert({ user.name, &user });
+	}
+	end = std::chrono::steady_clock::now();
+	elapsed_ms =
+		std::chrono::duration_cast<std::chrono::nanoseconds>
+		(end - begin);
+	std::cout << "create map: " << elapsed_ms.count() << " ms\n";
+
+	begin = std::chrono::steady_clock::now();
+	auto vec_tmp = std::find(
+		database.begin(),
+		database.end(),
+		User{ count,"user" + std::to_string(count), true, 12345 }
+	);
+	end = std::chrono::steady_clock::now();
+	elapsed_ms =
+		std::chrono::duration_cast<std::chrono::nanoseconds>
+		(end - begin);
+	std::cout << "Find in vector: " <<
+		elapsed_ms.count() << " ms\n";
+
+	begin = std::chrono::steady_clock::now();
+	auto map_tmp = searchTree.find("user" + std::to_string(count));
+	end = std::chrono::steady_clock::now();
+	elapsed_ms =
+		std::chrono::duration_cast<std::chrono::nanoseconds>
+		(end - begin);
+	std::cout << "Find in map: " <<
+		elapsed_ms.count() << " ms\n";
+}
 
 std::map<std::string, User> g_Users;
 
@@ -43,52 +120,65 @@ int main() {
 	
 	// Vector
 
-	int count = 10000;
+	//task(1'000'000);
 
-	std::vector < User > myVector;
-	myVector.reserve(count);
-	auto begin = std::chrono::steady_clock::now();
-	for (std::size_t i = 0; i < myVector.capacity(); i++) {
-		myVector.emplace_back(i + 1, "User" + std::to_string(i + 1));
+	std::vector <User> Users;
+	Users.reserve(100);
+
+	for (int i = 0; i < Users.capacity(); i++)
+	{
+		Users.emplace_back(i + 1, "user" + std::to_string(i + 1), static_cast<bool>(::rand() % 2), 32000 + ::rand());
 	}
-	auto end = std::chrono::steady_clock::now();
-	auto elapsed_ms = std::chrono::duration_cast <std::chrono::milliseconds>
-		(end - begin);
-	std::cout << "create vector: " << elapsed_ms.count() << " ms\n";
 
-	// Map
+	std::cout << "Id:\t" << "Name:\t" << "dayShift:\t" << "salary:\t\n";
 
-	std::map < std::string, User* > myMap;
-	
-	begin = std::chrono::steady_clock::now();
+	int avarageSalary = 0;
 
-	for (auto& user : myVector) {
-		myMap.insert({ user.name, &user });
+	for (auto& el : Users) {
+
+		std::cout << el.id << "\t";
+		std::cout << el.name << "\t";
+		std::cout << el.dayShift << "\t\t";
+		std::cout << el.salary << "\t";
+		std::cout << std::endl;
+		avarageSalary += el.salary;
+		
 	}
-	end = std::chrono::steady_clock::now();
-	elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds> (end - begin);
+	avarageSalary = avarageSalary / 100;
+
+	std::cout << "\nAverage salary: " << avarageSalary << std::endl;
+
+	// Finding 3 users near average Salary
+
+	for (auto& el : Users) {
+		el.temp = (double)(el.salary - avarageSalary);
+		if(el.temp > 2000 && el.temp < 2600)
+		std::cout << "Name: " << el.name << " " << "Salary: " << el.salary << "\n";
+	}
+
+	std::ofstream out;          // поток для записи
+	out.open("C:\\Users\\З - 3\\source\\repos\\Перекрасов Р\\15.08.2022 Map\\15.08.2022 Map\\hello.txt"); // открываем файл для записи
 	
-	std::cout << "create map: " << elapsed_ms.count() << " ms\n";
+	for (auto& el : Users) {
+		if (out.is_open())
+		{
+			out << el.id << "\t";
+			out << el.name << "\t";
+			out << el.dayShift << "\t\t";
+			out << el.salary << "\t";
+			out << std::endl;
+		}
+		else
+			std::cout << "Error openning file!\n";
 
-	begin = std::chrono::steady_clock::now();
-	auto vec_tmp = std::find(
-		myMap.begin(),
-		myMap.end(),
-		User{ count, "user" + std::to_string(count) }
-	);
+	
+	}
+	std::cout << "End of program" << std::endl;
 
-	end = std::chrono::steady_clock::now();
-	elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>
-		(end - begin);
-	std::cout << "Find in vector: " <<
-		elapsed_ms.count() << " ms\n";
+	// Using Multimap
 
-	begin = std::chrono::steady_clock::now();
-	auto map_tmp = myMap.find("user" + std::to_string(count));
-	end = std::chrono::steady_clock::now();
-	elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>
-		(end - begin);
-	std::cout << "Find in map: " << elapsed_ms.count() << " ms\n";
+
+
+	return 0;
+
 }
-
-
